4666a60dcb2a5016a7fb25699f79c48e21dc25eb
manuelcalerosolis <mcalero@gestool.es>
2014-04-10 01:03:05 +0200

    Limpiando el controlador

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -5,6 +5,7 @@ class RegisterController extends BaseController {
     /**
      * Instantiate a new UserController instance.
      */
+
     public function __construct()
     {
         $this->beforeFilter(function()
@@ -16,69 +17,59 @@ class RegisterController extends BaseController {
     public function Check()
     {
 
-    	$conditions = array( 
-    		'email' => 'required|email|unique:users',
-            'name' => 'required|unique:users',
-            'password' => 'required|min:8|confirmed',
+        $conditions = array( 
+            'email'                 => 'required|email|unique:users',
+            'name'                  => 'required|unique:users',
+            'password'              => 'required|min:8|confirmed',
             'password_confirmation' => 'required'
 
+        );
 
-    	);
-
-		$messages = array(
-            'email.required' => Lang::get('messages.email_required'),
-            'email.email' => Lang::get('messages.email_email'),
-            'email.unique' => Lang::get('messages.email_unique'),
-		    'name.required' => Lang::get('messages.name_required'),
-            'name.unique' => Lang::get('messages.name_unique'),
-            'password.required' => Lang::get('messages.password_required'),
-            'password.min' => Lang::get('messages.password_min'),
-            'password.confirmed' => Lang::get('messages.password_confirmed'),
+        $messages = array(
+            'email.required'                 => Lang::get('messages.email_required'),
+            'email.email'                    => Lang::get('messages.email_email'),
+            'email.unique'                   => Lang::get('messages.email_unique'),
+            'name.required'                  => Lang::get('messages.name_required'),
+            'name.unique'                    => Lang::get('messages.name_unique'),
+            'password.required'              => Lang::get('messages.password_required'),
+            'password.min'                   => Lang::get('messages.password_min'),
+            'password.confirmed'             => Lang::get('messages.password_confirmed'),
             'password_confirmation.required' => Lang::get('messages.password_confirmation')
-		);
+        );
 
 		$validator = Validator::make( Input::all(), $conditions, $messages );    	
 
 		if ($validator->fails())
 		{
-     	
      		return Redirect::intended('register')
                 ->withErrors($validator) 
                 ->withInput();
         }
         else
         {
-
-            $user               = new User;
-            $user->email        = Input::get('email');
-            $user->name         = Input::get('name');
-            $user->password     = Hash::make(Input::get('password'));
-            $user->save();
-            
-            Mail::send('emails.confirmation', Input::all(), function($message)
-            {
-                $message->to(Input::get('email'), Input::get('name'))->subject('Welcome!');
-            });
-            
+            $this->createUser();
+            $this->sendMailConfirmation();    
             return 'Registrado';
         }
-     
-
-        // $email      = Input::get('email');
-        // $password   = Input::get('password');
-        // //$password   = Hash::make($password);
-
-        //  if ( Auth::attempt(array('email' => $email, 'password' => $password)))
-        //  {
-        //      return "Bienvenido";
-        //  }
-        //  else
-        //  {
-        //     
-        
-
     }
 
+    public function createUser()
+    {
+        $user               = new User;
+        $user->email        = Input::get('email');
+        $user->name         = Input::get('name');
+        $user->password     = Hash::make(Input::get('password'));
+        $user->save();
+    }
+
+    public function sendMailConfirmation()
+    {
+        Mail::send('emails.confirmation', Input::all(), function($message)
+        {
+            $message->to(Input::get('email'), Input::get('name'))->subject('Welcome!');
+        });       
+    }
+     
 }
 
 ?>
