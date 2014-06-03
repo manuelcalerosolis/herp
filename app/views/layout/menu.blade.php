9d538d10bca25b9138f6cd18e39dc99cbe851097
Alvaro Sauci <alvaro.sauci@gmail.com>
2014-06-03 20:36:15 +0200

    optimizacion login

--- app/views/layout/menu.blade.php
+++ app/views/layout/menu.blade.php
@@ -19,7 +19,7 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse navbar-ex1-collapse">
 				<ul class="nav navbar-nav side-nav">
-					<li class="active"><a href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					<li class="active"><a href=" {{ URL::route('dashboard') }} "><i class="fa fa-dashboard"></i> Dashboard</a></li>
 					<li class="dropdown">
 				  		<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-caret-square-o-down"></i> Dropdown <b class="caret"></b></a>
 						<ul class="dropdown-menu">
