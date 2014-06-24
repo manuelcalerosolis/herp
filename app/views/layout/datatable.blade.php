7fbf8575c9ce5b6e8ee70bd184f2337ccb6244a1
manuelcalerosolis <mcalero@gestool.es>
2014-06-24 18:12:04 +0200

    datatable

--- app/views/layout/datatable.blade.php
+++ app/views/layout/datatable.blade.php
@@ -35,26 +35,28 @@
 
 </table>
 
-
 <script type="text/javascript">
     jQuery(document).ready(function(){
         // dynamic table
         jQuery('.{{ $class }}').dataTable({
             "bAutoWidth": false,
             @if (isset($hasCheckboxes) && $hasCheckboxes)
-            'aaSorting': [['1', 'asc']],
-            // Disable sorting on the first column
-            "aoColumnDefs": [ {
-            'bSortable': false,
-            'aTargets': [ 0, {{ count($columns) - 1 }} ]
-    } ],
-    @endif
+                'aaSorting': [['1', 'asc']],
+                // Disable sorting on the first column
+                "aoColumnDefs": [ {
+                    'bSortable': false,
+                    'aTargets': [ 0, {{ count($columns) - 1 }} ]
+                    } ],
+            @endif
+
     @foreach ($options as $k => $o)
     {{ json_encode($k) }}: {{ json_encode($o) }},
     @endforeach
+
     @foreach ($callbacks as $k => $o)
     {{ json_encode($k) }}: {{ $o }},
     @endforeach
+
     "fnDrawCallback": function(oSettings) {
         if (window.onDatatableReady) {
             window.onDatatableReady();
